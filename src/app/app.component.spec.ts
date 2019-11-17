a68b5f3093ada70d9dc2a68c6253ad1208500ec4
Jim van Zuidam <j.vanzuidam@student.avans.nl>
2019-11-17 21:57:39 +0100

    Renew of project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('BeveiligingsBedrijfAvans');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to BeveiligingsBedrijfAvans!');
+    expect(compiled.querySelector('.content span').textContent).toContain('BeveiligingsBedrijfAvans app is running!');
   });
 });
